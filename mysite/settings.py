05a123d688826c4915ed1e40730949efa8dc6e0d
Ariel Vernaza <ariel.darkserver@gmail.com>
2019-01-01 21:47:00 -0500

    fixing issue

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,8 +59,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = './static/'
+MEDIA_URL = './media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
