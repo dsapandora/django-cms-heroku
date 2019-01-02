3bc1c8a385b00ae3783c336ee94a93b242634be1
Ariel Vernaza <ariel.darkserver@gmail.com>
2019-01-01 21:40:19 -0500

    default

--- mysite/settings.py
+++ mysite/settings.py
@@ -175,7 +175,17 @@ CMS_PERMISSION = True
 
 CMS_PLACEHOLDER_CONF = {}
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'CONN_MAX_AGE': 0,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'HOST': 'localhost',
+        'NAME': 'project.db',
+        'PASSWORD': '',
+        'PORT': '',
+        'USER': ''
+    }
+}
 
 MIGRATION_MODULES = {
     
