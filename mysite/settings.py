213d07f69541181881b015fe44bc922d558cdfed
Ariel Vernaza <ariel.darkserver@gmail.com>
2019-01-01 22:15:41 -0500

    fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -60,7 +60,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
